ba148fad56ca9200d4c8fe36fe9077ff0f3c14e0
Michael <michael.chen880@gmail.com>
2017-07-18 11:16:52 -0700

    Building an app to get info on github users using the github api. create-react-app, cleaned up unnecessary files. This is a practice app

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+const API = 'https://api.github.com';
+//let url = `${API}/${username}`
+
+// axios.get('/user', {
+//     params: {
+//       ID: 12345
+//     }
+//   })
+//   .then(function (response) {
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+//axios.get(url[, config])
+
+class App extends Component {
+
+
+
+    render() {
+        return (
+            <div>
+                <h1>GIT GIT GIT GIT</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
